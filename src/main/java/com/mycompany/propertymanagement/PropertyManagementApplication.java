0ee9d631e4389e36a3029a1e4b9b15826eeef06a
Sarathparasaram <sarathchandra@ensarsolutions.com>
2023-03-14 10:34:51 +0530

    initial commit

---
+++ src/main/java/com/mycompany/propertymanagement/PropertyManagementApplication.java
@@ -0,0 +1,16 @@
+package com.mycompany.propertymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyManagementApplication {
+
+	public PropertyManagementApplication() {
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyManagementApplication.class, args);
+	}
+
+}
